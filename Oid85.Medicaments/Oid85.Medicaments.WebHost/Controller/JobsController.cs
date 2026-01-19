d23d7ac0602283015b1921228d19ad93bb8da9f5
oid85 <oid85@mail.ru>
2026-01-19 13:21:25 +0700

    .

---
+++ Oid85.Medicaments/Oid85.Medicaments.WebHost/Controller/JobsController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Oid85.Medicaments.Application.Interfaces.Services;
+using Oid85.Medicaments.Core;
+using Oid85.Medicaments.Core.Requests;
+using Oid85.Medicaments.Core.Responses;
+using Oid85.Medicaments.WebHost.Controller.Base;
+
+namespace Oid85.Medicaments.WebHost.Controller;
+
+/// <summary>
+/// Задачи по расписанию
+/// </summary>
+[Route("api/jobs")]
+[ApiController]
+public class JobsController(
+    IJobService jobService)
+    : BaseController
+{
+    /// <summary>
+    /// Обновить остатки лекарств
+    /// </summary>
+    [HttpPost("update-reserve")]
+    [ProducesResponseType(typeof(BaseResponse<GetMedicamentListResponse>), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(BaseResponse<GetMedicamentListResponse>), StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(typeof(BaseResponse<GetMedicamentListResponse>), StatusCodes.Status500InternalServerError)]
+    public async Task<IActionResult> GetMedicamentListAsync(
+        [FromBody] GetMedicamentListRequest request)
+    {
+        await jobService.UpdateReserveAsync();
+        return Ok();
+    }
+}
